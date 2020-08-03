8f9353cf059133833036dec787994eb8c5a98cf3
Jonathan <eimerarts@gmail.com>
2020-08-02 19:30:33 -0500

    write to html

--- Profile.js
+++ Profile.js
@@ -2,12 +2,40 @@ const fs = require('fs');
 
 function generatePage(data) {
     return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>My Team</h1>
+    </header>
     <div class="card-deck">
         <div class="card col-2">
           <div class="card-body">
             <h4 class="card-title">${data.name}</h4>
             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
           </div>
-        </div>`
+        </div>
+        <div class="card col-2">
+          <div class="card-body">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+          </div>
+        </div>
+        <div class="card col-2">
+          <div class="card-body">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+          </div>
+        </div>
+      </div>
+         
+</body>
+</html>
 }
 module.exports = generatePage;
